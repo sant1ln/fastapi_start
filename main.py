0523ce084ddd1c952e7a3b13a8518aadcb82f51d
Santiago s-code2 <santiagoln09@gmail.com>
2023-04-04 15:46:58 -0500

    requests

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -26,6 +26,20 @@ def home():
 
 ## Creating of Request and Response body
 @app.post('/person/new')
-def create_client(person: Person = Body):
+def create_client(person: Person = Body(...)):
     return person
 
+# Validations query params
+@app.get('/person/detail')
+def show_client(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)  
+):
+    return {name: age}
+
+# Validaciones path parameters
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(..., gt=0)
+):
+    return {person_id: 'It existsss'}
