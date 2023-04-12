3bd976969cfb6f7402afcfe75fe146f67e6cabcd
Santiago s-code2 <santiagoln09@gmail.com>
2023-04-11 21:33:46 -0500

    First model

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm.session import sessionmaker
 #Manipular bases de datos
 from sqlalchemy.ext.declarative import declarative_base
 
-sqlite_file_name = "database.sqlite"
+sqlite_file_name = "../database.sqlite"
 
 # Leer directorio actual
 base_dir = os.path.dirname(os.path.realpath(__file__))
